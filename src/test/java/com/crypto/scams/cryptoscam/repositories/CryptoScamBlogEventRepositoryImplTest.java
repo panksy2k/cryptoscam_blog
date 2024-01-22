6831447a8d0fbb996008f4d7581c2026aed20246
Pankaj Pardasani <pankaj.d.p@gmail.com>
2024-01-22 10:57:25 +0000

    added css and other thymeleaf config for rendering html

--- src/test/java/com/crypto/scams/cryptoscam/repositories/CryptoScamBlogEventRepositoryImplTest.java
+++ src/test/java/com/crypto/scams/cryptoscam/repositories/CryptoScamBlogEventRepositoryImplTest.java
@@ -21,77 +21,79 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @ExtendWith(VertxExtension.class)
 public class CryptoScamBlogEventRepositoryImplTest {
 
-  private CryptoScamBlogEventRepositoryImpl SUT;
+    private CryptoScamBlogEventRepositoryImpl SUT;
 
-  @Container
-  private PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:11-alpine")
-    .withDatabaseName("cryptoscamdb").withUsername("pankaj").withPassword("secret");
+    @Container
+    private PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:11-alpine")
+            .withDatabaseName("cryptoscamdb").withUsername("pankaj").withPassword("secret");
 
-  @BeforeEach
-  void setup (Vertx vertx, VertxTestContext context) {
-    int port = container.getFirstMappedPort();
-    String uri = "postgresql://pankaj:secret@localhost:" + port + "/cryptoscamdb";
-    SqlClient client = PgPool.client(vertx, uri);
-    SUT = new CryptoScamBlogEventRepositoryImpl(vertx, client);
-    SUT.createCryptoScamBlogEventTable().onSuccess(r -> context.completeNow()).onFailure(context::failNow);
-  }
+    @BeforeEach
+    void setup(Vertx vertx, VertxTestContext context) {
+        int port = container.getFirstMappedPort();
+        String uri = "postgresql://pankaj:secret@localhost:" + port + "/cryptoscamdb";
+        SqlClient client = PgPool.client(vertx, uri);
+        SUT = new CryptoScamBlogEventRepositoryImpl(vertx, client);
+        SUT.createCryptoScamBlogEventTable().onSuccess(r -> context.completeNow()).onFailure(context::failNow);
+    }
 
-  @Test
-  public void testContainerIsRunningTest(){
-    Assertions.assertTrue(container.isRunning());
-  }
+    @Test
+    public void testContainerIsRunningTest() {
+        Assertions.assertTrue(container.isRunning());
+    }
 
-  @Test
-  public void testCreateCryptoScamEvent(VertxTestContext context) {
-    CryptoScamBlogEvent data = new CryptoScamBlogEvent(2L, "kucisev.vip scam", "There is this gang of people...", "http://somewebsite.co.uk", true,
-      Arrays.asList("kucoin", "kucisev", "crypto", "scam", "exchangescam"));
+    @Test
+    public void testCreateCryptoScamEvent(VertxTestContext context) {
+        CryptoScamBlogEvent data = new CryptoScamBlogEvent(2L, "kucisev.vip scam", "There is this gang of people...",
+                "http://somewebsite.co.uk", true,
+                Arrays.asList("kucoin", "kucisev", "crypto", "scam", "exchangescam"));
 
-    context.verify(() -> {
-        Future<CryptoScamBlogEvent> result = SUT.saveCryptoScamEvent(data);
-        result.onFailure(context::failNow);
-        result.onSuccess(blogEvent -> {
-            Assertions.assertEquals(2L, blogEvent.getId());
-            context.completeNow();
+        context.verify(() -> {
+            Future<CryptoScamBlogEvent> result = SUT.saveCryptoScamEvent(data);
+            result.onFailure(context::failNow);
+            result.onSuccess(blogEvent -> {
+                Assertions.assertEquals(2L, blogEvent.getId());
+                context.completeNow();
+            });
         });
-    });
-  }
+    }
 
-  @Test
-  public void testUpdateBlogEvent(VertxTestContext testContext) {
-    //Given
-    Checkpoint createCP = testContext.checkpoint();
-    Checkpoint updateCP = testContext.checkpoint();
-    CryptoScamBlogEvent initial =
-      new CryptoScamBlogEvent(1L, "Test Title", "Test Desc", "TEST REF", false, Arrays.asList("FRAUD"));
+    @Test
+    public void testUpdateBlogEvent(VertxTestContext testContext) {
+        //Given
+        Checkpoint createCP = testContext.checkpoint();
+        Checkpoint updateCP = testContext.checkpoint();
+        CryptoScamBlogEvent initial =
+                new CryptoScamBlogEvent(1L, "Test Title", "Test Desc", "TEST REF", false, Arrays.asList("FRAUD"));
 
-    //When
-    testContext.verify(() -> {
-      SUT.saveCryptoScamEvent(initial)
-        .map(saved -> {
-          long persistedId = saved.getId();
-          Assertions.assertEquals(1L, persistedId);
-          createCP.flag();
-          return saved;
-        })
-        .map(existing -> new CryptoScamBlogEvent(existing.getId(), existing.getTitle(), existing.getDescription(),
-          existing.getReference(), true, Arrays.asList("scam")))
-        .compose(changedEntry -> {
-          updateCP.flag();
-          return SUT.updateCryptoScamEvent(changedEntry);
-        })
-        .map(updatedEventBlog -> updatedEventBlog.getId())
-        .compose(id -> SUT.findCryptoScamEvent(id))
-        .map(retrived -> {
-          Assertions.assertTrue(retrived.isPresent());
-          CryptoScamBlogEvent cryptoScamBlogEvent = retrived.get();
-          Assertions.assertTrue(cryptoScamBlogEvent.getBlogActive());
-          Assertions.assertEquals(new String[]{"scam"}, cryptoScamBlogEvent.getTags());
-          return cryptoScamBlogEvent.getId();
-        })
-        .onSuccess(id -> {
-        })
-        .onFailure(id -> testContext.failNow(id));
-    });
+        //When
+        testContext.verify(() -> {
+            SUT.saveCryptoScamEvent(initial)
+                    .map(saved -> {
+                        long persistedId = saved.getId();
+                        Assertions.assertEquals(1L, persistedId);
+                        createCP.flag();
+                        return saved;
+                    })
+                    .map(existing -> new CryptoScamBlogEvent(existing.getId(), existing.getTitle(),
+                            existing.getDescription(),
+                            existing.getReference(), true, Arrays.asList("scam")))
+                    .compose(changedEntry -> {
+                        updateCP.flag();
+                        return SUT.updateCryptoScamEvent(changedEntry);
+                    })
+                    .map(updatedEventBlog -> updatedEventBlog.getId())
+                    .compose(id -> SUT.findCryptoScamEvent(id))
+                    .map(retrived -> {
+                        Assertions.assertTrue(retrived.isPresent());
+                        CryptoScamBlogEvent cryptoScamBlogEvent = retrived.get();
+                        Assertions.assertTrue(cryptoScamBlogEvent.getBlogActive());
+                        Assertions.assertEquals(new String[]{"scam"}, cryptoScamBlogEvent.getTags());
+                        return cryptoScamBlogEvent.getId();
+                    })
+                    .onSuccess(id -> {
+                    })
+                    .onFailure(id -> testContext.failNow(id));
+        });
 
-  }
+    }
 }
