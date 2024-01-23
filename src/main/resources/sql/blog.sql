a520d248bbaacc6b66813838842eedac662cc49a
Pankaj Pardasani <pankaj.d.p@gmail.com>
2024-01-23 09:09:44 +0000

    added linespace in template

--- src/main/resources/sql/blog.sql
+++ src/main/resources/sql/blog.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "crypto_scam_event" (
 event_id serial PRIMARY KEY,
 name varchar(25) NOT NULL,
-title varchar(50) NOT NULL,
+title varchar(100) NOT NULL,
 description varchar(2000) NOT NULL,
 other_reference_url varchar(100),
 is_active boolean DEFAULT TRUE,
